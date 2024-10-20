be101715e41751a49f83affa869cd743ca999f4c
mjsaboor <97687303+mjsaboor@users.noreply.github.com>
2024-10-20 15:16:30 +0000

    updating the OHI_app

--- OHI_app.py
+++ OHI_app.py
@@ -25,4 +25,31 @@ st.header("Select a Historical Figure")
 selected_name = st.selectbox("Choose a name:", dt['IntervieweeName'])
 if selected_name:
     person_info = dt[dt['IntervieweeName'] == selected_name].iloc[0]
-    st.write(f"معرفی: {person_info['Intro']}")
+    st.write(f"معرفی: {person_info['Intro  ']}")
+
+ # Section 3: Tabs for graphs/tables
+st.header("Data Visualization")
+tabs = st.tabs(["gender ratio", "country of education", "Profession Analysis"])
+
+with tabs[0]:
+    # Bar Chart for Experience Overview
+    st.subheader("Experience Overview")
+    country_counts = dt['University'].value_counts()
+    plt.figure(figsize=(10, 6))
+    sns.barplot(x=country_counts.index, y=country_counts.values, palette='viridis')
+    plt.title("Number of Occurrences for Each Country")
+    plt.xlabel("Country")
+    plt.ylabel("Count")
+    plt.xticks(rotation=45)  # Rotate x-axis labels for better readability
+    plt.tight_layout()  # Adjust layout to prevent clipping of tick-labels
+    plt.show()
+
+with tabs[1]:
+    # Pie Chart for Field Distribution
+    st.subheader("Field Distribution")
+
+
+with tabs[2]:
+    # Table of Professions
+    st.subheader("Profession Analysis")
+ 
